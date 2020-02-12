a58d432e5d45c636cdee29aa7f8b243ed5b9f73e
Kamil <tomala.kamil@gmail.com>
2020-02-12 12:37:11 +0100

    Add functions to substract,multiply,divide

---
+++ operation.js
@@ -0,0 +1,6 @@
+const add = (x, y) => +x + +y;
+const substract = (x, y) => x - +y;
+const multiply = (x, y) => +x * +y;
+const divide = (x, y) => +x / +y;
+
+module.exports = { add, substract, multiply, divide };
