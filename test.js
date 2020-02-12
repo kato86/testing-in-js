2e3ffb09bde0c6f140bd26be72d5a85bf08914a3
Kamil <tomala.kamil@gmail.com>
2020-02-12 12:48:08 +0100

    Add test validateNumbers

--- test.js
+++ test.js
@@ -30,6 +30,19 @@ if (
   })
 );
 
+//adding validation
+it("indicates failure when a string is used instead of a number", () => {
+  assert.equal(operation.validateNumbers("summy", 5), false);
+});
+
+it("indicates failure when two strings is used instead of numbers", () => {
+  assert.equal(operation.validateNumbers("summy", "summy"), false);
+});
+
+it("successfully runs when two numbers are used", () => {
+  assert.equal(operation.validateNumbers(5, 5), true);
+});
+
 // it("should return true", () => {
 //   assert.equal(true, true);
 // });
